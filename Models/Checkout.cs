bae39a1db78c92675fbd669a26d118d2b3872eed
Zavier Hopson <zo.hopson94@gmail.com>
2024-05-22 11:18:24 -0500

    models created

---
+++ Models/Checkout.cs
@@ -0,0 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+using Microsoft.Net.Http.Headers;
+
+namespace LoncotesLibrary.Models;
+
+public class Checkout
+{
+    public int Id { get; set; }
+    [Required]
+    public int MaterialId { get; set; }
+    public Material Material { get; set; }
+    [Required]
+    public int PatronId { get; set; }
+    public Patron Patron { get; set; }
+    [Required]
+    public DateTime CheckoutDate { get; set; }
+    public DateTime? ReturnDate { get; set; }
+}
---
+++ Models/Genre.cs
@@ -0,0 +1,7 @@
+namespace LoncotesLibrary.Models;
+
+public class Genre
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+}
