04ba09a03b0eb2f3815794280932b77966a42a39
Zavier Hopson <zo.hopson94@gmail.com>
2024-05-23 13:19:45 -0500

    updated new migrations'

---
+++ Migrations/20240523164229_Checkouts.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace LoncotesLibrary.Migrations
+{
+    /// <inheritdoc />
+    public partial class Checkouts : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Patrons",
+                columns: new[] { "Id", "Address", "Email", "FirstName", "IsActive", "LastName" },
+                values: new object[,]
+                {
+                    { 3, "489 Sicourax St", "zengloose@hotmail.com", "Imelda", false, "Zeng" },
+                    { 4, "6387 Cote D'Ivoire Ave", "kudzumustdie@invasive.com", "Eki", false, "Kurzmann" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Checkouts",
+                columns: new[] { "Id", "CheckoutDate", "MaterialId", "PatronId", "ReturnDate" },
+                values: new object[,]
+                {
+                    { 1, new DateTime(2024, 1, 31, 0, 0, 0, 0, DateTimeKind.Unspecified), 5, 3, new DateTime(2024, 2, 5, 0, 0, 0, 0, DateTimeKind.Unspecified) },
+                    { 2, new DateTime(2024, 5, 23, 0, 0, 0, 0, DateTimeKind.Unspecified), 8, 4, null }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Checkouts",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Checkouts",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Patrons",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Patrons",
+                keyColumn: "Id",
+                keyValue: 4);
+        }
+    }
+}
